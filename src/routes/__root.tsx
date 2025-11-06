d8fea7e7e38f801e235b7083f18e52f7c4f70212
Kitagawa19 <kkitagawa16219@gmail.com>
2025-11-06 23:00:23 +0900

    pathの修正(#5)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
-import { Header } from '../components/layout/Header'
+import { Header } from '@/components/layout/Header'
 
 export const Route = createRootRoute({
   component: () => (
