a6b6946e789b2d172fe8b919f2f14170f95ac9b9
Kitagawa19 <kkitagawa16219@gmail.com>
2025-11-10 16:36:36 +0900

    ライトモード＆ダークモードのボタン追加(#5)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,9 @@
 import  NavItem  from '@/components/ui/NavItem';
 import { Search, Settings } from 'lucide-react';
+import ThemeButton from '@/components/ui/ThemeButton';
 
 export function Header() {
-  const headerBgColor = 'bg-gray-800 dark:bg-gray-900'; 
+  const headerBgColor = 'bg-white dark:bg-gray-900'; 
   
   return (
     <header className={headerBgColor}>
@@ -10,14 +11,14 @@ export function Header() {
         
         {/* ロゴ部分 */}
         {/* ▼ 色のカスタマイズポイント (ロゴテキスト色) ▼ */}
-        <a href="/" className="px-4 text-xl font-bold text-white">
+        <a href="/" className="px-4 text-xl font-bold text-black dark:text-white">
           案内
         </a>
 
         {/* ナビゲーションリンク群 */}
         <div className="flex  ml-auto">
-          <NavItem  icon={Search} />
-          <NavItem  icon={Settings} />
+          <NavItem  icon={Search} onClick={() => {}} />
+          <ThemeButton />
         </div>
       </nav>
     </header>
