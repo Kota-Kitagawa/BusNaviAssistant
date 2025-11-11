7ebf98c8fdc302aa568bfd2c37f29a5e1426affb
KitagawaKouta <kkitagawa16219@gmail.com>
2025-11-11 15:00:12 +0900

    headerの除去(#3)

--- src/routes/$stopName/$busNumber/index.tsx
+++ src/routes/$stopName/$busNumber/index.tsx
@@ -1,11 +1,9 @@
 import { BusNumberRoute } from '@/routes/$stopName/$busNumber/root'
-import { Header } from '@/components/layout/Header'
 
 export function StopBusName() {
     const { busNumber } = BusNumberRoute.useParams() 
     return (
         <>
-            <Header />
             <h1>Bus Number: {busNumber}</h1>
         </>
     )
