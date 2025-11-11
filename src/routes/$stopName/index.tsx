807b8d61833ec7f161c0f4dca70ebec5786a9880
KitagawaKouta <kkitagawa16219@gmail.com>
2025-11-11 14:47:00 +0900

    インデント修正(#3)

--- src/routes/$stopName/index.tsx
+++ src/routes/$stopName/index.tsx
@@ -2,11 +2,11 @@ import { StopRoute } from '@/routes/$stopName/root'
 import { Header } from '@/components/layout/Header'
 
 export function StopBusName() {
-  const { stopName } = StopRoute.useParams() 
-  return (
-    <>
-      <Header />
-      <h1>Bus Stop: {stopName}</h1>
-    </>
-  )
+    const { stopName } = StopRoute.useParams()
+    return (
+        <>
+            <Header />
+            <h1>Bus Stop: {stopName}</h1>
+        </>
+    )
 }
