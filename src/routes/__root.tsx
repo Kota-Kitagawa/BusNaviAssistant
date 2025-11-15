c7c4fc4a398f71aa917b32d3e6886a9295d0eb2d
KitagawaKouta <kkitagawa16219@gmail.com>
2025-11-15 23:57:12 +0900

    チャットの表示を追加(#8)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,25 @@
-import { Outlet, createRootRoute,createRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Outlet, createRootRoute, createRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { TanStackDevtools } from '@tanstack/react-devtools';
 
-import { Header } from '@/components/layout/Header'
-import { Chat } from '@/components/layout/Chat'
-import type React from 'react'
+import { Header } from '@/components/layout/Header';
+import { Chat } from '@/components/layout/Chat';
+import type React from 'react';
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Header />
-      <Outlet />
+      <div className='md:hidden'>
+        <Header inSidebar={true} />
+      </div>
+
+      <main className="md:pt-0 md:ml-80 min-h-[calc(100vh-4rem)]">
+        <div className="max-w-7xl mx-auto">
+          <Outlet />
+        </div>
+      </main>
       <Chat />
+
       <TanStackDevtools
         config={{
           position: 'bottom-right',
@@ -25,14 +33,14 @@ export const Route = createRootRoute({
       />
     </>
   ),
-})
+});
 
 
 
-export function AddRoot(rootName:string,Component:React.ComponentType){
+export function AddRoot(rootName: string, Component: React.ComponentType) {
   const newRoute = createRoute({
     getParentRoute: () => Route,
-    path: `$${rootName}`, 
+    path: `$${rootName}`,
     component: () => <Component />,
   });
   return newRoute;
